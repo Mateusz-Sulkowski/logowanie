611bbf99b7271d7b6b25b4c4c5b2ec2459414a9a
Mateusz <mateusz.sulkowski.pl@gmail.com>
2022-02-04 17:50:52 +0100

    Szkielet projektu

---
+++ log.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Page Title</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <div id="nav">
+            Logowanie
+        </div>    
+    </body>
+</html> 
